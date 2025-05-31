cb05cb46e31afc0b4da99f2083c02cae185ae133
chmudasser1 <mmudasser212@gmail.com>
2025-05-31 22:02:26 +0500

    add

--- server/index.js
+++ server/index.js
@@ -25,8 +25,8 @@ connectUserdb(mongodburl);
 app.get("/", (req, res) => {
     res.send("WhatsApp.io backend is running!");
 });
-app.use("/api/", loginapi);
-app.use("/api/message", messageapi);
+app.use("api/", loginapi);
+app.use("api/message", messageapi);
 
 server.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
