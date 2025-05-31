8f392c92bb664e627e472025340f032f9c167f36
chmudasser1 <mmudasser212@gmail.com>
2025-05-31 23:15:04 +0500

    new

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 const port = process.env.PORT;
 app.use(express.json());
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin: 'https://whatsapp-io-yazl.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
